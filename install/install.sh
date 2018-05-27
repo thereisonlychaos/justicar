94ea291969251dc8a4d40317801f84304bebe0a7
thereisonlychaos <the_magnuspater@yahoo.com>
2018-05-27 16:08:18 -0500

    Use Node v8 on install

--- install/install.sh
+++ install/install.sh
@@ -1,2 +1,12 @@
+apt-get install curl
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get update
+echo "Installing IRC server"
 apt-get install inspircd
+echo "Installing nodejs"
+sudo apt-get install -y nodejs
+echo "Node Version Check (should be >= 8)"
+nodejs -v
+
+echo "Copying example environment file"
 cp example.env ../.env
